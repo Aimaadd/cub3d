4cbb43d1bec035c533b5a3e20b88787e0fc4d30f
Aimaadd <aimadbentaye@gmail.com>
2024-11-27 20:00:22 +0100

    reading the whole map in order to parse it

--- src/process_cub/parse_map.c
+++ src/process_cub/parse_map.c
@@ -6,66 +6,85 @@
 /*   By: abentaye <abentaye@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:07:15 by abentaye          #+#    #+#             */
-/*   Updated: 2024/11/15 16:59:00 by abentaye         ###   ########.fr       */
+/*   Updated: 2024/11/27 19:51:34 by abentaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-static int  map_infos(t_base *base)
-{
-    int i;
-    int j;
+// static int  map_infos(t_base *base)
+// {
+//     int i;
+//     int j;
     
-    i = 0;
-    j = 0;
-    while (base->data->map[i][j])
+//     i = 0;
+//     j = 0;
+//     while (base->data->map[i][j])
+//     {
+//         if (base->data->map[i][j] == 'N' || base->data->map[i][j] == 'S'
+//             || base->data->map[i][j] == 'W' || base->data->map[i][j] == 'E')
+//             {
+//                 base->player->pos_x = i;
+//                 base->player->pos_y = j;
+//                 base->player->dir = base->data->map[i][j];
+//                 return (0);
+//             }
+//         j++;
+//         if (base->data->map[i][j] == '\n')
+//         {
+//             i++;
+//             j = 0;
+//         }
+//     }
+//     return (error_handler("No player found"), 1);
+// }
+
+static int char_reader(char c, int i, int j, t_base *base)
+{
+    if (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'W' || c == 'E'
+        || c == '\n' || c == ' ')
     {
-        if (base->data->map[i][j] == 'N' || base->data->map[i][j] == 'S'
-            || base->data->map[i][j] == 'W' || base->data->map[i][j] == 'E')
-            {
-                base->player->pos_x = i;
-                base->player->pos_y = j;
-                base->player->dir = base->data->map[i][j];
-                return (0);
-            }
-        j++;
-        if (base->data->map[i][j] == '\n')
+        if (c == 'N' || c == 'S' || c == 'W' || c == 'E')
         {
-            i++;
-            j = 0;
+            base->map->character++;
+            base->player->dir = c;
+            base->player->pos_y = j;
+            base->player->pos_x = i;
         }
+        return (0);
     }
-    return (error_handler("No player found"), 1);
+    else
+        return (error_handler("Invalid character"), 1);   
+    return (1);
 }
 
-static int  read_map(char **map)
+static int read_map(t_base *base)
 {
     int i;
     int j;
-
     i = 0;
-    j = 0;
-    while (map[i])
+    while (base->data->map[i])
     {
-        if (map[j][i] != 'N' && map[j][i] != 'S' && map[j][i] != 'W'
-            && map[j][i] != 'E' && map[j][i] != '0' && map[j][i] != '1')
-            {
-                printf("%c i : %d, j : %d\n", map[j][i], i, j   );
-                return (error_handler("Invalid map"), 1);
-            }
-        if (map[j][i] == '\n')
+        j = 0;
+        while (base->data->map[i][j])
+        {
+            if (char_reader(base->data->map[i][j], i, j, base) == 1)
+                return (1);
             j++;
+        }
         i++;
     }
+    if (base->map->character > 1)
+        return (error_handler("Too many characters"), 1);
+    if (base->map->character == 0)
+        return (error_handler("No character found"), 1);
     return (printf("map is valid\n"), 0);
 }
-
 int valid_map(t_base *base)
 {
-    if (read_map(base->data->map) == 1)
+    if (read_map(base) == 1)
         return (1);
-    if (map_infos(base) == 1)
-        return (1);
+    // if (map_infos(base) == 1)
+    //     return (1);
     return (0);
 }
